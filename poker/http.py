acbb992577908f29d58194de674bf82921a0f11b
Isaac Evans <409041+ievans@users.noreply.github.com>
2020-09-28 17:27:43 -0700

    Update http.py

--- poker/http.py
+++ poker/http.py
@@ -16,6 +16,7 @@ from hashlib import sha256
 from poker import game
 
 with open_binary("poker", "index.html") as fh:
+    print('reading binary')
     SPA_CONTENTS = fh.read()
 
 with open_binary("poker", "chime.oga") as fh:
